5bfbc0d40f10fc8280740def79a7987236fa3166
TheusMarqs <matheusmarqsilva@gmail.com>
2023-04-11 15:37:30 -0300

    quinto commit

--- script.js
+++ script.js
@@ -19,9 +19,13 @@ function moedaDois(m2) {
 function Calcular() {
     var valor = parseFloat(document.getElementById('valor').value);
     var resultado = document.getElementById('resultado');
-    var cambio = parseFloat(document.getElementById('cambio').value);
-    var valorFinal = valor / cambio;
+    var cambioLibra = parseFloat(document.getElementById('cambioLibra').value);
+    var cambioDolar = parseFloat(document.getElementById('cambioDolar').value);
+    var cambioEuro = parseFloat(document.getElementById('cambioEuro').value);
+    var valorLibra = valor / cambioLibra;
+    var valorDolar = valor / cambioDolar;
+    var valorEuro = valor / cambioEuro;
 
-    resultado.value = valorFinal.toFixed(2);
+    resultado.value = 'Libra: ' + valorLibra.toFixed(2) + ' / Dólar: ' + valorDolar.toFixed(2) + ' / Euro: ' + valorEuro.toFixed(2);
 
 }
